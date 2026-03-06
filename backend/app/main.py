68f6d3e7d306d017d527ba8d8d5c6379a0d67a3b
NiteshSingla123 <nitesh9462980739@gmail.com>
2026-03-06 14:49:33 +0530

    Day2

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
-app = FastAPI(title="Cloud Native DevOps Platform")
+from . import models, schemas, crud
+from .database import engine, SessionLocal
 
-@app.get("/")
-def home():
-    return {"message": "DevOps Platform API Running"}
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db, user)
+
+
+@app.get("/users", response_model=list[schemas.UserResponse])
+def read_users(db: Session = Depends(get_db)):
+    return crud.get_users(db)
